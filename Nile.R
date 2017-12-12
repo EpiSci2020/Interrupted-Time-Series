c7a3e12542df9472080faa563642e5e86e57a129
David Nichols <epi.sci2020@gmail.com>
2017-12-12 00:07:00 -0500

    ITS through Visualization

--- Nile.R
+++ Nile.R
@@ -1,3 +1,12 @@
-data(Nile)
-nile <- Nile
 library(tidyverse)
+library(read_r)
+nile <- read_csv("nile.csv")
+str(nile)
+glimpse(nile)
+m1 <- lm(flow ~ year, data = nile)
+coef(m1)
+ggplot() +
+  geom_point(aes(x = year, y = flow), data = nile) + 
+  geom_line(aes(x = year, y = flow), data = nile) +
+  geom_abline(intercept = 42898, slope = -21, color = "blue")
+k
